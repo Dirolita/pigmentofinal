d06a99d277e13a15492d5f844698528417f4fc89
natalyq1 <onatylili@gmail.com>
2022-06-10 14:19:47 -0500

    added bootstrap, style components and fontawsome to index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cgaleria from "./components/cgaleria/Cgaleria";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
 <React.StrictMode>
-    
+   <Cgaleria /> 
   </React.StrictMode>
 );
 
